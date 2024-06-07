901475ea93c7b45ab31d8071bb1666d7dcd0d34c
kartolo727 <kartolo727@gmail.com>
2024-06-07 04:29:12 +0000

    Clo Fashion

--- script/java.js
+++ script/java.js
@@ -3,3 +3,17 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", this.window.scrollY > 0);
 })
+
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2119872112.
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navmenu.classList.remove('open');
+}
